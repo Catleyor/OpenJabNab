9d005e7ca10af7cb7c95ed18efee83bc47ad0ca0
RedoXPS <dev@redox.ws>
2011-08-23 01:25:51 +0200

    Add spaces around TTS_FOLDER constant, so it doesn't mess with Scite syntax highlighting anymore

--- server/lib/ttsinterface_inline.h
+++ server/lib/ttsinterface_inline.h
@@ -36,7 +36,7 @@ inline TTSInterface::TTSInterface(QString name, QString visualName):ttsName(name
 		folder.cd(name);
 	}
 	ttsFolder = folder;
-	ttsHTTPUrl = "broadcast/ojn_local/"TTS_FOLDER "/"+name+"/%1/%2"; // %1 For voice, %2 for FileName
+	ttsHTTPUrl = "broadcast/ojn_local/" TTS_FOLDER "/"+name+"/%1/%2"; // %1 For voice, %2 for FileName
 	// Compute TTS's Http path
 }
 
